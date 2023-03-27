799df42d2682b815e22897e49b4a96dbe3d87bb5
devcsb <devcsb119@gmail.com>
2023-03-27 23:23:59 +0900

    서블릿 등록
    ServletContextInitializer를 구현한 오브젝트(HttpServlet)를 ServletWebServerFactory의 getWebServer() 메서드에 전달하여 서블릿을 초기화한다.

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,12 +4,30 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+					response.setStatus(HttpStatus.OK.value());
+					response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+					response.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+
+		});
 		webServer.start();
 	}
 }
