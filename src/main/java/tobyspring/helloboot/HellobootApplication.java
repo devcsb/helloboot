f20bb5ab39f9692aebc8076924ccdecfee660c4e
devcsb <devcsb119@gmail.com>
2023-03-27 23:55:25 +0900

    프론트 컨트롤러로 전환
    직접 분기처리하여 프론트컨트롤러 구현

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -18,16 +19,27 @@ public class HellobootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                    String name = request.getParameter("name");
+                    //인증, 보안, 다국어처리 등 공통 기능을 처리
 
-                    response.setStatus(HttpStatus.OK.value());
-                    response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                    response.getWriter().println("Hello " + name);
+                    // 매핑 작업
+                    if (request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())){
+                        String name = request.getParameter("name");
+
+                        response.setStatus(HttpStatus.OK.value());
+                        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        response.getWriter().println("Hello " + name);
+                    }
+                    else if (request.getRequestURI().equals("/user")) {
+                        //
+                    }
+                    else {
+                        response.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*"); // 1.모든 요청을 받는다.
 
         });
         webServer.start();
