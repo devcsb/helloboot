95413d3c6e39186550574eedaadeb2d790376fa1
devcsb <devcsb119@gmail.com>
2023-04-05 00:03:13 +0900

    DispatcherServlet으로 전환
    모든 요청을 DispatcherSerlvet이 받도록 변경.

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,40 +20,17 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh(); // 초기화
+        applicationContext.refresh();
 
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-            servletContext.addServlet("frontController", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                    //인증, 보안, 다국어처리 등 공통 기능을 처리하는 코드가 위치할 부분...
-
-                    // 매핑 작업
-                    if (request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())){
-                        String name = request.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String returnValue = helloController.hello(name);
-
-                        response.setStatus(HttpStatus.OK.value()); // 생략해도 무방함.(서블릿에서 에러가 발생하지 않을시 200을 자동으로 내려주므로)
-                        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        response.getWriter().println(returnValue);
-                    }
-                    else if (request.getRequestURI().equals("/user")) {
-                        //
-                    }
-                    else {
-                        response.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
-
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+                    ).addMapping("/*");
         });
         webServer.start();
     }
