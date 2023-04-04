95060f7b7602e6e0bc1969f57368b25db36f3396
devcsb <devcsb119@gmail.com>
2023-04-04 22:31:41 +0900

    독립 실행형 스프링 애플리케이션 - Hello 컨트롤러 매핑과 바인딩

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,9 +18,13 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class); // 빈 등록
+        applicationContext.refresh(); // 초기화
+
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
 
             servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
@@ -30,6 +35,7 @@ public class HellobootApplication {
                     if (request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())){
                         String name = request.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String returnValue = helloController.hello(name);
 
                         response.setStatus(HttpStatus.OK.value()); // 생략해도 무방함.(서블릿에서 에러가 발생하지 않을시 200을 자동으로 내려주므로)
