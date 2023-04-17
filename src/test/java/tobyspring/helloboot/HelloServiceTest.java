015e7e700126046f2820dbf6c51ab8912c3439cf
devcsb <devcsb119@gmail.com>
2023-04-17 23:13:21 +0900

    DI를 이용한 Decorator, Proxy 패턴
    구현체가 아닌 인터페이스에 의존하여 의존관계의 방향을 아래에서 위로 설계하는 방식의 장점,
     데코레이터 패턴과 프록시 패턴의 개념, @Primary 어노테이션으로 우선순위 설정하는 방법 등

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -1,6 +1,7 @@
 package tobyspring.helloboot;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,5 +15,13 @@ public class HelloServiceTest {
         String ret = helloService.sayHello("Test");
 
         assertThat(ret).isEqualTo("Hello Test");
+    }
+
+    @Test
+    void helloDecorator() {
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+
+        String ret = decorator.sayHello("Test");
+        Assertions.assertThat(ret).isEqualTo("*Test*");
     }
 }
