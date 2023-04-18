8163320875535771d62abe9000f048127459cc63
devcsb <devcsb119@gmail.com>
2023-04-18 21:53:09 +0900

    합성 애노테이션의 적용
    직접 만든 합성 애노테이션을 적용해보기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,25 +1,9 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan // 해당 패키지와 하위 패키지에 있는 모든 클래스들 중에서 @Component 어노테이션이 붙은 클래스들을 스캔하여 스프링 컨텍스트에 빈으로 등록한다
+@MySpringBootAnnotation
 public class HellobootApplication {
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
---
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,16 @@
+package tobyspring.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // default 는 RetentionPolicy.CLASS
+@Target(ElementType.TYPE) // TYPE : Class, interface (including annotation type), or enum declaration
+@Configuration
+@ComponentScan // 해당 패키지와 하위 패키지에 있는 모든 클래스들 중에서 @Component 어노테이션이 붙은 클래스들을 스캔하여 스프링 컨텍스트에 빈으로 등록한다
+public @interface MySpringBootAnnotation {
+}
