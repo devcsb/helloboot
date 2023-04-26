da312cc690a34f75d160189f96c376118ec5178e
devcsb <devcsb119@gmail.com>
2023-04-26 20:56:50 +0900

    인프라 빈 구성정보의 분리
    @Import 를 사용하여 스캔 대상이 아닌 클래스를 빈으로 등록하는 방법을 이용하여 구성정보 분리

--- src/main/java/tobyspring/config/MySpringBootApplication.java
+++ src/main/java/tobyspring/config/MySpringBootApplication.java
@@ -1,7 +1,8 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,5 +13,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE) // TYPE : Class, interface (including annotation type), or enum declaration
 @Configuration
 @ComponentScan // 해당 패키지와 하위 패키지에 있는 모든 클래스들 중에서 @Component 어노테이션이 붙은 클래스들을 스캔하여 스프링 컨텍스트에 빈으로 등록한다
-public @interface MySpringBootAnnotation {
+@EnableMyAutoConfiguration
+public @interface MySpringBootApplication {
 }
