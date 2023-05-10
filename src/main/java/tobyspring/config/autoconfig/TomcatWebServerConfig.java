5249e88cd25335c8227fe964347e4e0f2656a686
devcsb <devcsb119@gmail.com>
2023-05-10 11:06:49 +0900

    스프링 부트의 @Conditional
    다양한 @Conditional 어노테이션의 종류 학습

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -21,4 +21,7 @@ public class TomcatWebServerConfig {
 * @Configuration 클래스 레벨의 @ConditionalOnClass 와 @Bean 메소드 레벨의 @ConditionalOnMissingBean 조합은 가장 대표적으로 사용되는 방식.
 * 클래스 레벨의 검증 없이 @Bean 메소드에만 적요하면 불필요하게 @Configuration 클래스가 빈으로 등록되기 때문.
 *
+* @ConditionalOnMissingBean은 현재까지 컨테이너에 등록된 빈 정보를 기준으로 체크를 하기 때문에,
+* 커스텀 빈 구성 정보에 이 어노테이션을 적용하는 것은
+* 피해야한다. (구성 정보 처리 순서가 커스텀 빈 구성 정보 -> 자동 구성 정보 이므로, 커스텀 구성 정보에 적용 시, 어떤 커스텀 구성 정보가 먼저 적용되었는지 명확히 알 수 없기 때문)
 * */
