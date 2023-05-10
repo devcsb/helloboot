f22b70ae5e40b55749b5abf04508306ce8aba47c
devcsb <devcsb119@gmail.com>
2023-05-10 13:59:33 +0900

    @Value와 PropertySourcesPlaceholderConfigurer
    @Value 애노테이션과 placeholer로 프로퍼티 값 사용하는 방법 소개.
    @Value로 프로퍼티 값 교체를 위해서 후처리 작업을 하는 PropertySourcesPlaceholderConfigurer에 대한 설명

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -12,13 +13,30 @@ import tobyspring.config.MyAutoConfiguration;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat") // 1. 클래스의 존재로 해당 기술의 사용 여부를 확인하고
 public class TomcatWebServerConfig {
+
+    // @Value의 치환자를 프로퍼티 값으로 교체하려면 PropertySourcesPlaceholderConfigurer 타입의 빈을 등록해줘야 한다.
+    @Value("${contextPath}")
+    String contextPath;
+
+    // @Value로 값을 가져오는 방식
+    @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 2. 개발자가 직접 추가한 커스텀 빈 구성 중, 해당 메서드의 반환타입과 일치하는 빈이 있는지 확인하여, 일치하는 빈이 없을 때만 구성정보 적용
+    public ServletWebServerFactory servletWebServerFactory() {
+        System.out.println("contextPath = " + contextPath);
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(this.contextPath); // 모든 서블릿 매핑 앞에 contextPath 추가
+        return factory;
+    }
+
+/*    //Environment에서 값을 가져오는 방식
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 2. 개발자가 직접 추가한 커스텀 빈 구성 중, 해당 메서드의 반환타입과 일치하는 빈이 있는지 확인하여, 일치하는 빈이 없을 때만 구성정보 적용
     public ServletWebServerFactory servletWebServerFactory(Environment env) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         factory.setContextPath(env.getProperty("contextPath")); // 모든 서블릿 매핑 앞에 contextPath 추가
         return factory;
-    }
+    }*/
+
 }
 
 /*
